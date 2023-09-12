70ef63ea44d976639e9730aabf41201ce47000e6
s3927562 <s3927562@rmit.edu.vn>
2023-09-12 18:36:45 +0700

    Initial Commit

---
+++ Group 17 Firebase/Group_17_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_17_FirebaseApp.swift
+//  Group 17 Firebase
+//
+//  Created by Tung Tran Thanh on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Group_17_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
